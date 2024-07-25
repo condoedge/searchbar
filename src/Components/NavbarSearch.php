6a76981eeff3c161cf67073ec3021469244783fb
Ezequiel <tavelliezequiel@gmail.com>
2024-07-25 19:24:44 -0300

    Adding components

---
+++ src/Components/NavbarSearch.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Kompo\Menus;
+
+use Kompo\Form;
+use Kompo\Searchbar\Components\SearchKomponentUtils;
+use Kompo\Searchbar\Components\SearchStateRequestUtils;
+
+class NavbarSearch extends Form
+{
+    use SearchStateRequestUtils;
+    use SearchKomponentUtils;
+    
+    public $class = 'relative flex-1 flex-row items-center';
+
+    public $id = 'navbar-search';
+
+    public function render()
+    {
+        return _Rows(
+            _Rows(
+                _Sax('search-normal-1', 24)->class('text-greenmain mt-0 pt-0 px-2'),
+                !$this->state->getSearchableInstance()?->searchableName() ? null : _Flex(
+                    _Html($this->state->getSearchableInstance()?->searchableName())->asPill()->class('bg-level4'),
+                    ...$this->state->getRules()->map(fn($rule, $i) => $rule->render($i)) 
+                )->class('gap-4 px-2 mr-3'),
+                _Input()->name('search')->class('navbar-search-input')
+                    ->default($this->state->getSearch())
+                    ->placeholder('crm.search')
+                    ->class('w-full mb-0 text-xl [&>.vlInputWrapper:focus-within]:shadow-none')
+                    ->inputClass('py-2')
+                    ->noAutocomplete()
+                    ->onFocus(fn($e) => $e->selfGet('openSearch')->refresh('search-panel'))
+                    ->onInput(fn($e) => $e->selfPost('setSearch')->refresh('search-panel')),
+            )->class('w-full flex-row items-center focus-within:border border-greenmain rounded-lg'),
+
+            _Rows(
+                $this->instanciateSearchKomponent(SearchPanel::class),
+            )->id('search-panel')->class('fixed top-14 md:top-full left-0 md:absolute z-[110] w-screen md:w-full'),
+        )->class('nav-search-box flex-1');
+    }
+}
---
+++ src/Components/SearchPanel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Kompo\Menus;
+
+use Kompo\Form;
+use Kompo\Searchbar\Components\CustomFiltersModal;
+use Kompo\Searchbar\Components\EnhancedSearchbar;
+use Kompo\Searchbar\Components\SearchKomponentUtils;
+use Kompo\Searchbar\Components\SearchStateRequestUtils;
+
+class SearchPanel extends Form
+{
+    use SearchStateRequestUtils;
+    use SearchKomponentUtils;
+
+    public $id = 'search-panel';
+
+    public function render()
+    {
+        if (!$this->state->isOpen()) {
+            return null;
+        }
+
+        $searchableI = $this->state->getSearchableInstance();
+
+        return _Flex(
+            $searchableI ? $this->sections($searchableI) : _Rows(
+                searchService()->optionsSearchables()
+            )->class('p-4'),
+
+            _Rows(
+                _FlexEnd(
+                    _Link()->icon('x')->class('text-3xl mb-3 mt-1 absolute top-0')->selfPost('closeSearch')->refresh(),
+                ),
+
+                _Rows(
+                    $this->instanciateSearchKomponent(EnhancedSearchbar::class),
+                )->class('items-start pr-10')
+            )->class('flex-1 overflow-y-auto mini-scroll !pb-2 relative pl-4 py-4')->style('height:380px')
+        )->alignStart()
+        ->class('w-screen md:w-full bg-white rounded-b-2xl border border-gray-200 shadow-xl border-b border-l border-r border-greenmain px-2 py-2');
+    }
+
+    protected function sections($searchableI)
+    {
+        return _Rows(
+            _Link('filter.back')->icon(_sax('arrow-left',20))->selfGet('getBack')->refresh('navbar-search')->class('mt-4 border-b py-4 border-level4 text-black font-semibold'),
+            _Rows($searchableI->decoratedSections()->map(fn($s) => $s->showOptions()->class('border-b py-4 border-level4'))),
+            _Link('filter.custom-filters')->class('text-black font-semibold py-4')
+                ->selfGet('getCustomFiltersModal')->inModal(),
+        )->class('px-4');
+    }
+
+    public function getCustomFiltersModal()
+    {
+        return $this->instanciateSearchKomponent(CustomFiltersModal::class);
+    }
+}
